ac945f2ea7a26c422735a64eb46c414b158cf533
Rainette <rainette.engbers@epfl.ch>
2026-05-07 16:24:12 +0200

    update utils

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -5,3 +5,5 @@ epsilon = 0.622  # Ratio of molecular weights (Mw/Md = 18.016/28.964)
 R_dry_air = 287.05  # Specific gas constant for dry air [J kg^-1 K^-1]
 R_w = 461.5  # Specific gas constant for water vapor [J kg^-1 K^-1]
 c_p = 1005 # specific heat of air [J kg^-1 K^-1]
+sigma = 5.670374e-8 # Stefan-Boltzmann constant [W m^-2 K^-4]
+ems = 0.98 # Emissivity of the surface (assumed for Tskin
